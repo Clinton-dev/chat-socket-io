d2bea97420d026dac0c5439b5e0dde06835da2c5
clinton-dev <clintonwambugu@gmail.com>
2024-01-30 16:18:02 +0300

    Add chat api routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routers/userRoute");
+const chatRoute = require("./Routers/chatRoute");
 
 const app = express();
 require("dotenv").config();
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
